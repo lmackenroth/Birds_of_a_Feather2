bc3cd3cf60e81149c992e075599d094c06385576
lmackenroth <lmackenroth@smu.edu>
2024-12-15 13:15:54 -0600

    switches more smoothly but is very fast, fixing that

--- src/gradiant.ts
+++ src/gradiant.ts
@@ -7,7 +7,9 @@ export default class gradiants {
   private p5: P5Lib;
   private current: number = 0; //current gradiant Index
   private lastSwitch: number = 0; //  when the gradiant  switched las
-  private switchInterval: number = 5000; // Interval for switching gradients (ms)
+  private switchInterval: number = 50000; // Interval for switching gradients (ms), how long each gradiant stays up
+  private transition: number = 2000; // Duration of the gradient transition, tracking how long to wait before it starts to transition
+
   public colors: { color1: P5Lib.Color, color2: P5Lib.Color }[] = [];
 
   //contructor
@@ -26,6 +28,23 @@ export default class gradiants {
     }
 
   }
+
+  //be able to call next color via index
+
+  private getNextIndex(): number {
+    //next index modulus length of color array
+    return (this.current + 1) % this.colors.length;
+  }
+
+  private transitionProg(): number {
+    //get current time
+    const currentTime = this.p5.millis();
+    //time between now and the last time a gradiant switched out
+    const timeFrame = currentTime - this.lastSwitch;
+    //constrains the amount of time since last switch/ how long it takes to transition
+    return this.p5.constrain(timeFrame / this.transition, 0, 1);
+
+  }
   //switch the gradiant
   private switch(): void {
     const currentTime = this.p5.millis();
@@ -38,21 +57,33 @@ export default class gradiants {
   //rendor for gradient colors, i need to figure out how to cycle between the colors in the array
   render() {
 
-    //call gradiant switch
-    this.switch();
-    // pull out the colors again
-    const { color1, color2 } = this.colors[this.current];
+    const progress = this.transitionProg();
+    const nextIndex = this.getNextIndex();
+
+
+    // pull out the colors from current gradiant
+    const { color1: currColor1, color2: currColor2 } = this.colors[this.current];
+    //pull colors from next gradient
+    const { color1: nextColor1, color2: nextColor2 } = this.colors[nextIndex];
 
 
 
 
     for (let y = 0; y < this.p5.height; y++) {
       const inter = this.p5.map(y, 0, this.p5.height, 0, 1);
+      const blend1 = this.p5.lerpColor(currColor1, currColor2, progress);
+      const blend2 = this.p5.lerpColor(nextColor1, nextColor2, progress);
+
+      const c = this.p5.lerpColor(blend1, blend2, inter);
       //blend both colors based off height
-      const c = this.p5.lerpColor(color1, color2, inter);
       this.p5.stroke(c);
       this.p5.line(0, y, this.p5.width, y);
 
+    }
+    //call gradiant switch
+
+    if (progress >= 1) {
+      this.switch();
     }
 
 
